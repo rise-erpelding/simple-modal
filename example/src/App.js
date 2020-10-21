0a0fd845c33f2ba11ad5051c7ae675710fa6d3ff
Rise <rise.erpelding@gmail.com>
2020-10-21 10:22:56 -0500

    update example to test development

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,19 @@ import SimpleModal from 'simple-modal'
 import 'simple-modal/dist/index.css'
 
 class App extends Component {
+
   render() {
-    return (<SimpleModal show={false}><div>Hello modal</div></SimpleModal>)
+    const modalStyle = {
+      position: 'fixed',
+      background: 'white',
+      width: '80%',
+      height: '200px',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      border: '2px solid black'
+    };
+    return (<SimpleModal show={false}><div style={modalStyle}>Hello modal</div></SimpleModal>)
   }
 }
 
